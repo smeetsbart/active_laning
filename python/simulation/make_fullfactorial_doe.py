10708b7305d488dabf2bc1fbf6b2e2443d7fae0a
Bart Smeets <bart.smeets@kuleuven.be>
2021-02-10 10:10:40 +0100

    Added script to generate a full factorial design layout

---
+++ python/simulation/make_fullfactorial_doe.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python-mpacts
+import DEMutilities.multisim_layout as ms
+import DEMutilities.postprocessing.h5storage as h5s
+import DEMutilities.doe_samples as doe
+import numpy as  np
+
+#Simple example of a design of experiments script where we generate a full factorial design
+#and make the according simulation layout.
+
+f = h5s.H5Storage( "pstudy.h5", openmode='wa' )
+section = f.data_section( "doe", overwrite=False)
+
+sampler = doe.FullFactorialDesign( section )
+
+sampler.add_parameter("params/h" , doe.ValueList( np.linspace( 1.0,3.0, 5 ) ) )
+sampler.add_parameter("params/f_n", doe.ValueList( np.linspace( 0.15,3.,5 ) ) )
+
+#Three random repeats for EACH case
+sampler.add_full_repeat( "params/rng_seed", doe.ValueList(range( 3 )), unit='-', axis_label='sample' )
+
+sample_section = sampler.generate()
+
+ms.generate_layout( sample_section )
